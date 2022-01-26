191895759e07546ee999b3e97406bda779f0d4fb
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-01-26 08:15:28 -0500

    Initial project set up.

---
+++ src/main/java/com/alphacoder/TradeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeConfigServerApplication.class, args);
+	}
+
+}
